3311e67e3ce4e2a7adab8751c555678592093d76
RainPearls <rainpearlmagic@gmail.com>
2026-09-10 14:18:00 -0700

    class declaration and test to see if working

---
+++ add/AddTwo.java
@@ -0,0 +1,14 @@
+package add;
+
+/*
+    Name: Rayna
+    Partner A: Min Myat Thu
+    Partner B: Bao Tran
+*/
+
+public class AddTwo {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = l1;
+        return result;
+    }
+}
