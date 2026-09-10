e3e8d34de8deeed2c3eead380c4fb3bc04290d3c
Bao Tran <168223975+tranbao0@users.noreply.github.com>
2026-09-09 22:52:10 -0700

    Solution for Integer to Roman
    This was the fastest solution I could think of. I tried doing a LUT but it was for some reason slower on LC?

--- Roman/Solution.java
+++ Roman/Solution.java
@@ -1,3 +1,5 @@
+// import java.util.Scanner;
+
 class Solution {
     
     /*
@@ -6,6 +8,26 @@ class Solution {
      */
 
     public String intToRoman(int num) {
-        
+        StringBuilder value = new StringBuilder();
+
+        final int[] NUM= {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}; 
+        final String[] ROMAN= {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < NUM.length && num > 0; i++) {
+            while (num >= NUM[i]) {
+                value.append(ROMAN[i]);
+                num -= NUM[i];
+            }
+        }
+        return value.toString();
     }
+
+    /* 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(solution.intToRoman(scanner.nextInt()));
+        scanner.close();
+    }
+    */
 }
